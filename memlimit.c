29a29cbdcd96bcab1dd925621588d190efda2727
petergeelhoed <petergeelhoed@gmail.com>
2022-04-07 12:47:24 +0200

    better print

--- memlimit.c
+++ memlimit.c
@@ -9,11 +9,12 @@ void print_rlimit(struct rlimit *r, const char *name) {
     int64_t max;                /* Hard limit */
     cur = r->rlim_cur;
     max = r->rlim_max;
-    printf("%17s :rlim_cur => %16lld, :rlim_max => %16lld\n",
+    printf("%-17s rlim_cur => %12lld, rlim_max => %12lld\n",
            name, cur, max);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 	char* p;
 	int c;
 	int verbose = 0;
@@ -69,10 +70,26 @@ int main(int argc, char *argv[]) {
         {
             case 'h':
                 fprintf(stderr,"\
+sets the soft RLIMIT_AS to the -n specified value and runs array assignments\n\
+of -k specified size until the RLIMIT_AS memory runs out\n\
+The number of allocations is reported\n\n\
+$ ulimit -Sv 5000\n\
+would result in a \"Killed\" when memory ran out\n\
+$ memlimit -n 256000000\n\
+will report the number allocation\n\
 -n <size of addresspace in bytes>\n\
 -w <microseconds sleep>\n\
 -k <char array size> (1000)\n\
-verbose -v\n\
+-v : verbose, print all limits\n\n\
+Example:\n\
+$ ulimit -Sv 500000\n\
+$ memlimit -n 256000000\n\
+BEFORE:\n\
+RLIMIT_AS         rlim_cur =>    512000000, rlim_max =>   4294967295\n\
+AFTER:\n\
+RLIMIT_AS         rlim_cur =>    256000000, rlim_max =>   4294967295\n\
+\n\
+252243 allocations\n\
 ");
                 return 0;
             case 'v':
@@ -97,30 +114,34 @@ verbose -v\n\
 
 	getrlimit(9 ,&rlim); // RLIMIT_AS
 	printf("BEFORE:\n");
-	print_rlimit(&rlim, "9 AS");
-    for (i = 0; i < nl && verbose; i++) {
-        getrlimit(resources[i], &rlim);
-        print_rlimit(&rlim, names[i]);
-    }
-	rlim.rlim_cur = n;
-	if (setrlimit(9 ,&rlim) == 0)
+
+	if (verbose)
 	{
-		printf("\nAFTER:\n");
-		getrlimit(9 ,&rlim); // RLIMIT_AS
-		print_rlimit(&rlim, "9 AS");
-		for (i = 0; i < nl && verbose; i++) {
+		for (i = 0; i < nl; i++) {
 			getrlimit(resources[i], &rlim);
 			print_rlimit(&rlim, names[i]);
 		}
+	}
+	else
+	{
+		print_rlimit(&rlim, "RLIMIT_AS");
+	}
+	rlim.rlim_cur = n;
+
+	if (setrlimit(9 ,&rlim) == 0)
+	{
+		printf("AFTER:\n");
+		getrlimit(9 ,&rlim); // RLIMIT_AS
+		print_rlimit(&rlim, "RLIMIT_AS");
 		p++;
 		while (p != NULL) 
 		{
 			p=(char*)malloc(k);
 			j++;
 			if (wait) usleep(wait); // if you want to see this in htop
-			if (j%10000==0)printf("%d allocations\n",j);
+			if (j%10000==0 && verbose)printf("%d allocations of char[%d]\n",j,k);
 		}
-		printf("%d allocations\n",j);
+		printf("%d allocations of char[%d]\n",j,k);
 	}
 	else
 	{
